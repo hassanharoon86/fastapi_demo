fd83940ec64b54a8a4bb08f92a64f58f149ff7e3
Hassan Haroon <hassanharoon86@gmail.com>
2024-09-07 03:28:50 +0500

    CRUD with SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class Post(BaseModel):
   title: str
   content: str
   published: bool = True
-  rating: Optional[int] = None
+  # rating: Optional[int] = None
 
 while True:
   try:
@@ -52,44 +52,47 @@ async def root():
   return {'message': "Welcome to my API!"}
 
 @app.get('/posts')
-async def posts():
-  cursor.execute('SELECT * FROM posts;')
-  posts = cursor.fetchall()
+async def posts(db: Session = Depends(get_db)):
+  posts = db.query(models.Post).all()
   return {'data': posts}
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post, db: Session = Depends(get_db)):
-  cursor.execute('INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *', 
-                 (post.title, post.content, post.published))
+  post = models.Post(**post.model_dump())
   
-  new_post = cursor.fetchone()
-  conn.commit()
-  return {'data': new_post}
+  db.add(post)
+  db.commit()
+  db.refresh(post)
+  return {'data': post}
 
 @app.get('/posts/{id}')
-async def get_post(id: int):
-  post =   find_post(int(id))
+async def get_post(id: int, db: Session = Depends(get_db)):
+  post = db.query(models.Post).filter(models.Post.id == id).one()
   if not post:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                         detail=f"Post with id: {id} not found")
   return {'post': post}
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int):
-  cursor.execute('DELETE FROM posts WHERE id = %s RETURNING *;', (id, ))
-  post = cursor.fetchone()
-
-  if post is None:
+async def delete_post(id: int, db: Session = Depends(get_db)):
+  posts = db.query(models.Post).filter(models.Post.id == id)
+  if posts.first() is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-  conn.commit()
+
+  posts.delete(synchronize_session=False)
+  db.commit()
   return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put('/posts/{id}')
-async def update_post(id: int, post: Post):
-  cursor.execute('UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;',
-                  (post.title, post.content, post.published, id))
-  post = cursor.fetchone()
-  conn.commit()
+async def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+  post_query = db.query(models.Post).filter(models.Post.id == id)
+
+  post = post_query.first()
+
   if post is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not updated")
+
+  post_query.update(updated_post.model_dump(), synchronize_session=False)
+  db.commit()
+  db.refresh(post)
   return {'data': post}
